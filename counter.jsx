1a9a852addc7aeeaeea384d65025bd434473f7a0
26dhruv <58438228+26dhruv@users.noreply.github.com>
2022-05-10 13:54:10 +0530

    react
    adding two files

---
+++ counter.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom'
+class Counter extends Component { 
+    render() { 
+        return <h1>Hello world </h1>;
+    }
+}
+ 
+export default Counter ;
