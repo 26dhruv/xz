e28ce1ca0f046d963b21ce749e6db7aea7c223a2
26dhruv <58438228+26dhruv@users.noreply.github.com>
2022-05-10 14:03:01 +0530

    restoring to default

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.css"
 import Counter from './components/counter';
-ReactDOM.render(<Counter /> , document.getElementById('root'));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
